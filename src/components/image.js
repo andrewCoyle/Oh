3e284810a7f7ada1c948146db8b05c602476f3bf
aufimtsev <fimak@bk.ru>
2016-08-20 00:55:04 +0600

    tags on the image

--- src/components/image.js
+++ src/components/image.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import _ from 'lodash'
 import '../style/image.styl'
 
 export default class Image extends Component {
+
+  static propTypes = {
+    tags: PropTypes.array
+  }
+
+  static defaultProps = {
+    tags: [
+      {
+        id: 1,
+        left: 100,
+        top: 100
+      },
+      {
+        id: 2,
+        left: 250,
+        top: 250
+      }
+    ]
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      tags: this.props.tags
+    }
+  }
+
+  _createTag = (e) => {
+    let tags = this.state.tags
+    const id = tags.length + 1
+    const element = {
+      id: id,
+      left: e.pageX - 15,
+      top: e.pageY - 15
+    }
+    tags.push(element)
+
+    this.setState({
+      tags
+    })
+  }
+
   render() {
+    const { tags } = this.state
+
     return (
-      <div className="image-wrapper">
+      <div className="image-wrapper" onClick={this._createTag}>
         <div className="image">
           image
+
+          {_.map(tags, (tag) =>
+            this.renderTag(tag)
+          )}
+
         </div>
+      </div>
+    )
+  }
+
+  renderTag(tag) {
+    return (
+      <div
+        className="tag"
+        key={tag.id}
+        style={{
+          left: tag.left,
+          top: tag.top
+        }}
+      >
+        {tag.id}
       </div>
     )
   }
